9c7cc75737fcafa7412e544c043f161e78c0812d
Kadecahe <kadecahe@gmail.com>
2020-12-01 13:40:00 -0500

    Feat: Updated Readme

--- client/components/welcome.js
+++ client/components/welcome.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Welcome() {
   return (
     <main role="main" className="inner cover">
-      <h1 Name="cover-heading">Welcome to Steam.</h1>
+      <h1 Name="cover-heading">Welcome to Stream.</h1>
       <p className="lead">
         Stream is an application that allows you to choose a mood for
-        meditation, and will return a playlist to play on your spotify device.
+        meditation, and will return a playlist to play on your Spotify device.
         To start, log in with Spotify on our navigation bar and make sure to
-        have spotify open in your backgorund first! The application was built to
-        test Spotify's Web API. In its next stage, the application will work
-        with iphone devices.
+        have Spotify running on your background first! The application was built
+        to test Spotify's Web API. In its next stage, the application will work
+        with iPhone devices.
       </p>
     </main>
   )
